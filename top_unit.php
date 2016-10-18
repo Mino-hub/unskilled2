211f1bfed3ed3bc8fc05079f03c8bdac07c7dfc4
pecodrive <pecodrive@gmail.co>
2016-10-18 17:33:06 +0900

    ap

--- top_unit.php
+++ top_unit.php
@@ -1,6 +1,5 @@
 <div class="sd">
 <style>
-.ad_top { width: 336px; height: 280px; }
 @media(max-width: 319px) { .ad_top { width: 300px; height: 250px; } }
 @media(max-width: 320px)and(max-width: 355px){ .ad_top { width: 300px; height: 250px; } }
 @media(min-width: 356px)and(max-width: 425px) { .ad_top { width: 336px; height: 280px; } }
