b7df236a15cd6e465ed465584800c75e2c3b24f6
pecodrive <pecodrive@gmail.co>
2016-10-26 13:02:01 +0900

    adfix

--- top_unit.php
+++ top_unit.php
@@ -4,9 +4,9 @@
 @media(max-width: 320px)and(max-width: 355px){ .ad_top { width: 300px; height: 250px; } }
 @media(min-width: 356px)and(max-width: 425px) { .ad_top { width: 336px; height: 280px; } }
 @media(min-width: 426px)and(max-width: 769px) { .ad_top { width: 336px; height: 280px; } }
-@media(min-width: 769px)and(max-width: 1024px) { .ad_top { width: 336px; height: 280px; } } 
-@media(min-width: 1025px)and(max-width: 1366px){ .ad_top { width: 336px; height: 280px; } } 
-@media(min-width: 1367px){ .ad_top { width: 336px; height: 280px; } }
+@media(min-width: 769px)and(max-width: 1024px) { .ad_top { width: 728px; height: 90px; } } 
+@media(min-width: 1025px)and(max-width: 1366px){ .ad_top { width: 728px; height: 90px; } } 
+@media(min-width: 1367px){ .ad_top { width: 728px; height: 90px; } }
 </style>
 <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
 <!-- unskilled.site記事上レスポンシブ -->
